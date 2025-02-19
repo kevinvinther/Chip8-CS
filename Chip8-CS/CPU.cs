0b469431ed593ae0cd63c9304db1a99109e93ca0
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2025-02-19 10:00:32 +0100

    feat(cpu): add remaining variables

--- Chip8-CS/CPU.cs
+++ Chip8-CS/CPU.cs
@@ -3,4 +3,25 @@ namespace Chip8_CS;
 public class Cpu
 {
     private ushort _opcode;
+    // _v defines the general purpose registers named V0, V1, ..., VE.
+    private byte[] _v = new byte[16];
+    // index register
+    private ushort _i;
+    // program counter
+    private ushort _pc;
+    // graphics, if 0 the pixel is off, if 1 the pixel is on.
+    private byte[] _gfx = new byte[64 * 32];
+
+    // interrupts and hardware registers. counts at 60 hz. when above 0, counts down to zero.
+    private byte _delay_timer;
+    private byte _sound_timer;
+
+    // the stack is used to remember the current location before a jump is performed.
+    // thus you store the program counter before jumping. 
+    private ushort[] _stack = new ushort[16];
+    // stack pointer, holds the current level of stack (out of 16)
+    private ushort _sp;
+
+    // hex-based keypad (0x0-0xF). stores the current key state.
+    private byte[] _key = new byte[16];
 }
